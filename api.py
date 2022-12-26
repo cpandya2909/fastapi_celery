8596ecc4d8db3de162865ce03fd4bcd8267b6999
cpandya2909 <cpandya2909@gmail.com>
2022-12-26 22:37:27 +0530

    Update api.py
    Fix input datatypes.

--- api.py
+++ api.py
@@ -1,27 +1,32 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path
 from celery import Celery
 from celery.result import AsyncResult
+from pydantic import BaseModel
 
 app = FastAPI()
 
 # Initialize Celery
 celery = Celery(broker='redis://localhost:6379', backend='redis://localhost:6379')
 
-@app.post("/run-command")
-async def run_command(command: str):
+# model to get command as POST input
+class Command(BaseModel):
+    command: str
+
+@app.post("/run-command/")
+async def run_command(command: Command):
     # Validate the input command
     if not command:
         raise HTTPException(status_code=400, detail="Command is required")
-    
+    print(command.command)
     # Run the command as a Celery task
-    task = celery.send_task('tasks.run_command', args=[command])
+    task = celery.send_task('tasks.run_command', args=[command.command])
     
     return {
         "task_id": task.id
     }
 
-@app.get("/taks/{task_id")
-async def get_output(task_id: str):
+@app.get("/taks/{task_id}")
+async def get_output(task_id: str = Path(title= "Task id")):
     # Validate the input command
     if not task_id:
         raise HTTPException(status_code=400, detail="Task id is required")
